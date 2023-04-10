17b0669efd33422f520a06d19e450eb23c8cf37d
김명준 <kmj@itsdcode.com>
2023-04-10 13:37:26 +0900

    (feat): 디코트 웹뷰 구현, 외부링크를 앱 안에서 띄우기 구현

--- MyDcode/App.tsx
+++ MyDcode/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import WebApp from './WebApp';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+export type AppStackNavigationParams = {
+  Main: {root: string};
+};
+
+const Stack = createNativeStackNavigator<AppStackNavigationParams>();
 
 function App(): JSX.Element {
   return (
     <SafeAreaView style={StyleSheet.absoluteFill}>
-      <WebApp root={'https://www.itsdcode.com'} />
+      <Stack.Navigator
+        initialRouteName={'Main'}
+        screenOptions={{headerTintColor: '#82888'}}>
+        <Stack.Screen
+          name={'Main'}
+          component={WebApp}
+          initialParams={{root: 'https://www.itsdcode.com'}}
+        />
+      </Stack.Navigator>
     </SafeAreaView>
   );
 }
--- MyDcode/WebApp.tsx
+++ MyDcode/WebApp.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import WebView from 'react-native-webview';
+import {AppStackNavigationParams} from './App';
+import {RouteProp} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 interface Props {
-  root: string;
+  navigation: NativeStackNavigationProp<AppStackNavigationParams, 'Main'>;
+  route: RouteProp<AppStackNavigationParams, 'Main'>;
 }
 
-const WebApp: React.FC<Props> = ({root}) => {
-  return <WebView source={{uri: root}} />;
+const WebApp: React.FC<Props> = ({route, navigation}: Props) => {
+  return (
+    <WebView
+      source={{uri: route.params.root}}
+      decelerationRate="normal"
+      setSupportMultipleWindows={false}
+      onShouldStartLoadWithRequest={request => {
+        if (route.params.root.startsWith('https://support.itsdcode.com')) {
+          return true;
+        }
+
+        if (request.url.startsWith('https://support.itsdcode.com')) {
+          navigation.push('Main', {root: request.url});
+          return false;
+        }
+        return true;
+      }}
+    />
+  );
 };
 
 export default WebApp;
