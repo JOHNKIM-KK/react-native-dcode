0f5a9daca550061fb67cfded89759fa9b2c591a7
김명준 <kmj@itsdcode.com>
2023-04-22 20:19:06 +0900

    (feat): ActivityIndicator 가 webView 로딩시 보여지도록 구현

--- MyDcode/WebApp.tsx
+++ MyDcode/WebApp.tsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import WebView from 'react-native-webview';
 import {AppStackNavigationParams} from './App';
 import {RouteProp} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {ActivityIndicator, StyleSheet, View} from "react-native";
 
 interface Props {
   navigation: NativeStackNavigationProp<AppStackNavigationParams, 'Main'>;
   route: RouteProp<AppStackNavigationParams, 'Main'>;
 }
 
+
+const ActivityIndicatorElement = () => {
+    return (
+        <View style={styles.activityIndicatorStyle}>
+        <ActivityIndicator
+            color="grey"
+            size="large"
+        />
+        </View>
+    );
+};
+
+
 const WebApp: React.FC<Props> = ({route, navigation}: Props) => {
+    const [visible, setVisible] = useState<boolean>(false);
+
+
   return (
+      <>
     <WebView
       source={{uri: route.params.root}}
       decelerationRate="normal"
       setSupportMultipleWindows={false}
+      onLoadStart={() => setVisible(true)}
+      onLoad={() => setVisible(false)}
       onShouldStartLoadWithRequest={request => {
         if (route.params.root.startsWith('https://support.itsdcode.com')) {
           return true;
@@ -27,7 +47,27 @@ const WebApp: React.FC<Props> = ({route, navigation}: Props) => {
         return true;
       }}
     />
+          {visible ? <ActivityIndicatorElement /> : null}
+      </>
   );
 };
+
+
+const styles = StyleSheet.create({
+    activityIndicatorStyle: {
+        flex: 1,
+        position: 'absolute',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 'auto',
+        marginBottom: 'auto',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        justifyContent: 'center',
+    },
+});
+
 
 export default WebApp;
