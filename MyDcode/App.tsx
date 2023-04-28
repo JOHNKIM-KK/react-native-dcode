d232c9036b0e85a34c9b77708cdec1d46c9bc0c8
김명준 <kmj@itsdcode.com>
2023-04-28 14:33:44 +0900

    (feat): prettier, eslint 설정

--- MyDcode/App.tsx
+++ MyDcode/App.tsx
@@ -1,35 +1,32 @@
-import React, {useEffect} from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import WebApp from './WebApp';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Splash from "./Splash";
-import SplashScreen from 'react-native-splash-screen'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Splash from './Splash';
+import SplashScreen from 'react-native-splash-screen';
 
 export type AppStackNavigationParams = {
   Splash: undefined;
-  Main: {root: string};
+  Main: { root: string };
 };
 
 const Stack = createNativeStackNavigator<AppStackNavigationParams>();
 
 function App(): JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1500);
+  }, []);
 
-    useEffect(() => {
-        setTimeout(()=>{
-            SplashScreen.hide();
-        },1500)
-    }, []);
-
-    return (
+  return (
     <SafeAreaView style={StyleSheet.absoluteFill}>
-      <Stack.Navigator
-        initialRouteName={'Splash'}
-        screenOptions={{headerTintColor: '#82888'}}>
+      <Stack.Navigator initialRouteName={'Splash'} screenOptions={{ headerTintColor: '#82888' }}>
         {/*<Stack.Screen name={'Splash'} component={Splash} options={{headerShown:false}}/>*/}
         <Stack.Screen
           name={'Main'}
           component={WebApp}
-          initialParams={{root: 'https://www.itsdcode.com'}}
+          initialParams={{ root: 'https://www.itsdcode.com' }}
         />
       </Stack.Navigator>
     </SafeAreaView>
